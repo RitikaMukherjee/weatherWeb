0e954ec90b8fde0e482c5675a13fe57bbe8d6df8
RITIKA MUKHERJEE <60706878+RitikaMukherjee@users.noreply.github.com>
2021-11-19 13:29:26 +0530

    main.js

--- main.js
+++ main.js
@@ -2,12 +2,14 @@ const api = {
     key: "06804f5008bfa32b813f6ff2bf8eeea1",
     base: "https://api.openweathermap.org/data/2.5/"
   }
-  
   const searchbox = document.querySelector(".searchBox");
   searchbox.addEventListener('keypress', setQuery);
   
+  getResults("siliguri");
+  getResults2("siliguri");
+  
   function setQuery(evt) {
-    if (evt.keyCode == 13) {
+    if (evt.keyCode === 13) {
       getResults(searchbox.value);
       getResults2(searchbox.value);
     }
@@ -48,15 +50,15 @@ const api = {
   }
   function displayResults2(forecast){
     let day1 = document.querySelector('.forecast .day1');
-    day1.innerText = `day 1: ${Math.round(forecast.list[4].main.temp)-273}°c  ${forecast.list[4].weather[0].main}`;
+    day1.innerText = `day 1:\n\n${Math.round(forecast.list[4].main.temp)-273}°c\n${forecast.list[4].weather[0].main}`;
     let day2 = document.querySelector('.forecast .day2');
-    day2.innerText = `day 2: ${Math.round(forecast.list[12].main.temp)-273}°c  ${forecast.list[12].weather[0].main}`
+    day2.innerText = `day 2:\n\n${Math.round(forecast.list[12].main.temp)-273}°c\n${forecast.list[12].weather[0].main}`
     let day3 = document.querySelector('.forecast .day3');
-    day3.innerText = `day 3: ${Math.round(forecast.list[20].main.temp)-273}°c  ${forecast.list[20].weather[0].main}`
+    day3.innerText = `day 3:\n\n${Math.round(forecast.list[20].main.temp)-273}°c\n${forecast.list[20].weather[0].main}`
     let day4 = document.querySelector('.forecast .day4');
-    day4.innerText = `day 4: ${Math.round(forecast.list[28].main.temp)-273}°c  ${forecast.list[28].weather[0].main}`
+    day4.innerText = `day 4:\n\n${Math.round(forecast.list[28].main.temp)-273}°c\n${forecast.list[28].weather[0].main}`
     let day5 = document.querySelector('.forecast .day5');
-    day5.innerText = `day 5: ${Math.round(forecast.list[34].main.temp)-273}°c  ${forecast.list[34].weather[0].main}`
+    day5.innerText = `day 5:\n\n${Math.round(forecast.list[34].main.temp)-273}°c\n${forecast.list[34].weather[0].main}`
   }
   function dateBuilder (d) {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
